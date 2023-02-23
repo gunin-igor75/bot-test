85e6632c26eb9719b2135129b5e8a7f5f8a2f6e1
Igor Gunin <gunin-igor75@yandex.ru>
2023-02-23 08:03:36 +0600

    add value in pet

---
+++ src/main/resources/db/changelog/v-2.0/01-delete-pet-value.sql
@@ -0,0 +1,7 @@
+delete from pet where name='bob';
+delete from pet where name='tom';
+delete from pet where name='jerry';
+delete from pet where name='dog';
+delete from pet where name='cat';
+delete from pet where name='mouse';
+delete from pet where name='ball';
---
+++ src/main/resources/db/changelog/v-2.0/01-insert-pet-value.sql
@@ -0,0 +1,7 @@
+insert into pet (name, adopted) values ('bob', false);
+insert into pet (name, adopted) values ('tom', false);
+insert into pet (name, adopted) values ('jerry', false);
+insert into pet (name, adopted) values ('dog', false);
+insert into pet (name, adopted) values ('cat', false);
+insert into pet (name, adopted) values ('mouse', false);
+insert into pet (name, adopted) values ('ball', false);
